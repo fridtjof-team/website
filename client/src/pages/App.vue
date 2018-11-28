27a6c9d166677310d97511e7461688453ec7f027
Sritam Patnaik <sritampatnaik@gmail.com>
2018-11-28 18:42:25 +0800

    Fixed styling issue

--- client/src/pages/App.vue
+++ client/src/pages/App.vue
@@ -25,9 +25,58 @@ export default {
 </script>
 
 <style lang="scss">
-body {
+#mainBody {
   background: #F6F9FF;
-  color: #42436A
+  color: #42436A;
+
+
+  .stf-select__container {
+    .stf-select__inner-wrapper {
+      background-color:   #F6F9FF;
+      box-shadow: 0 0 38px 20px rgba(45,31,80,0.07);
+      border-radius: 4px;
+          padding: 5px 10px;
+    }
+  }
+
+
+  .stf-select_opened {
+    .stf-select__options {
+      background-color: #fff;
+      border: 0px solid #e2e6e8;
+      -webkit-box-shadow: 0 52 66px 0 rgba(34,48,73,.2);
+      box-shadow: 0 52px 66px 0 rgba(34,48,73,.2);
+      padding: 5px 10px;
+      border-radius: 0px 0px 3px 3px;
+
+
+      .stf-select-option {
+        border: none;
+        border-radius: 3px;
+        
+        &.stf-select-option_selected {
+          background-color: #2e4369;
+          color: white;
+          font-weight: 400;
+        }
+      }
+    }
+    .stf-select__search-input{
+      input {
+        height: 54px;
+        font-size: 18px;
+        padding: 0px 20px;
+        &:focus {
+          background-color: #fff;
+          border: 0px solid #e2e6e8;
+          border-radius: 3px 3px 0px 0px;
+          -webkit-box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
+          box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
+          outline: none;
+        }
+      }
+    }   
+  }
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -39,53 +88,5 @@ body {
   .nav-bar-blue {
     padding: 10px 10%;
   }
-}
-
-.stf-select__container {
-  .stf-select__inner-wrapper {
-    background-color:   #F6F9FF;
-    box-shadow: 0 0 38px 20px rgba(45,31,80,0.07);
-    border-radius: 4px;
-        padding: 5px 10px;
-  }
-}
-
-
-.stf-select_opened {
-  .stf-select__options {
-    background-color: #fff;
-    border: 0px solid #e2e6e8;
-    -webkit-box-shadow: 0 52 66px 0 rgba(34,48,73,.2);
-    box-shadow: 0 52px 66px 0 rgba(34,48,73,.2);
-    padding: 5px 10px;
-    border-radius: 0px 0px 3px 3px;
-
-
-    .stf-select-option {
-      border: none;
-      border-radius: 3px;
-      
-      &.stf-select-option_selected {
-        background-color: #2e4369;
-        color: white;
-        font-weight: 400;
-      }
-    }
-  }
-  .stf-select__search-input{
-    input {
-      height: 54px;
-      font-size: 18px;
-      padding: 0px 20px;
-      &:focus {
-        background-color: #fff;
-        border: 0px solid #e2e6e8;
-        border-radius: 3px 3px 0px 0px;
-        -webkit-box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-        box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-        outline: none;
-      }
-    }
-  }   
 }
 </style>
