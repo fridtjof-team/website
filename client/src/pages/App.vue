50efefc2a0b37bfac0d9e26b80d9b559d2f49624
Sritam Patnaik <sritampatnaik@gmail.com>
2018-12-11 01:14:59 +0800

    scss changes

--- client/src/pages/App.vue
+++ client/src/pages/App.vue
@@ -90,6 +90,10 @@ export default {
         height: calc(2.25rem + 2px);
         padding: 12px;
         font-weight: 700;
+        white-space: nowrap; 
+        overflow: hidden;
+        text-overflow: ellipsis;
+
         .flag-icon {
           margin: 0 10px 0 5px;
           border-radius: 3px;
@@ -152,129 +156,6 @@ export default {
         }
       }
     }
-  }
-
-  .dropdown-input-field {
-    background-color:   #F6F9FF;
-    box-shadow: 0 0 38px 20px rgba(45,31,80,0.07);
-    border-radius: 4px;
-    padding: 5px 10px;
-
-    .selected-tag {
-      font-weight: 700;
-    }
-    &.open {
-      background-color: #fff;
-      border: 0px solid #e2e6e8;
-      border-radius: 3px 3px 0px 0px;
-      -webkit-box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-      box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-      outline: none;
-
-      .selected-tag {
-        display: none;
-      }
-    }
-    
-    .dropdown-toggle {
-      border: none;
-      .vs__actions {
-        display: none;
-      }
-      &:after {
-        display: none;
-      }
-    }
-    .dropdown-menu {
-      background-color: #fff;
-      border: 0px solid #e2e6e8;
-      -webkit-box-shadow: 0 52 66px 0 rgba(34,48,73,.2);
-      box-shadow: 0 52px 66px 0 rgba(34,48,73,.2);
-      padding: 5px 10px;
-      border-radius: 0px 0px 3px 3px;
-
-      li {
-        a {
-          font-size: 14px;
-          line-height: 16px;
-          padding: 12px 24px 12px 12px;
-          box-sizing: border-box;
-          cursor: pointer;
-          outline: none;
-          border-radius: 3px;
-        }
-        
-      }
-
-      .active {
-        border-radius: 3px;
-
-        a {
-          background: $color-dark-blue;
-          color: white;
-        }
-        &.highlight {
-          background: #2e4369;
-          color: white;
-        }
-      }
-
-      .highlight {
-        a {
-          background: rgba(135,138,163,.1);
-          color: inherit;
-        }
-      } 
-    }
-  }
-
-
-  .stf-select__container {
-    .stf-select__inner-wrapper {
-      background-color:   #F6F9FF;
-      box-shadow: 0 0 38px 20px rgba(45,31,80,0.07);
-      border-radius: 4px;
-          padding: 5px 10px;
-    }
-  }
-
-
-  .stf-select_opened {
-    .stf-select__options {
-      background-color: #fff;
-      border: 0px solid #e2e6e8;
-      -webkit-box-shadow: 0 52 66px 0 rgba(34,48,73,.2);
-      box-shadow: 0 52px 66px 0 rgba(34,48,73,.2);
-      padding: 5px 10px;
-      border-radius: 0px 0px 3px 3px;
-
-
-      .stf-select-option {
-        border: none;
-        border-radius: 3px;
-        
-        &.stf-select-option_selected {
-          background-color: #2e4369;
-          color: white;
-          font-weight: 400;
-        }
-      }
-    }
-    .stf-select__search-input{
-      input {
-        height: 54px;
-        font-size: 18px;
-        padding: 0px 20px;
-        &:focus {
-          background-color: #fff;
-          border: 0px solid #e2e6e8;
-          border-radius: 3px 3px 0px 0px;
-          -webkit-box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-          box-shadow: 0 20px 66px 0 rgba(34,48,73,.2);
-          outline: none;
-        }
-      }
-    }   
   }
 }
 #app {
